7492827ac558f7e483e7641eb28b2a438edbcc61
sohad-hossam <sohad95husam@gmail.com>
2024-02-23 01:03:36 +0200

    post-retrieval fixes

--- FeatureExtraction.py
+++ FeatureExtraction.py
@@ -4,6 +4,8 @@ from imports import *
 class FeatureExtraction:
     def __init__(self, UCTokens: set) -> None:
         self.count_vectorizer = CountVectorizer(vocabulary=UCTokens)
+        self.vocab_index = {word: idx for idx, word in enumerate(UCTokens)}
+        # UC_count_matrix, code_count_matrix
 
     # Latent Semantic Analysis.
     def LSA(self, tfidf_matrix_uc: np.ndarray, tfidf_matrix_code: np.ndarray, train_or_test: str) -> np.ndarray:
@@ -23,35 +25,39 @@ class FeatureExtraction:
             LSA_similraity_matrix = cosine_similarity(LSA_data_useCases, LSA_data_codes)
             # print(LSA_similraity_matrix)
             return LSA_similraity_matrix
+    
+    def CountVectorizerModel(self, UC_documents: list, code_documents: list, train_or_test: str):
         
+        self.code_count_matrix = np.zeros((len(code_documents), len(code_documents[0])))
+        self.UC_count_matrix = np.zeros((len(UC_documents),len(UC_documents[0])))
+        
+        if train_or_test == 'train':
+            self.UC_count_matrix = self.count_vectorizer.fit_transform(UC_documents)
+            self.code_count_matrix = self.count_vectorizer.fit_transform(code_documents)
+        else:
+            self.UC_count_matrix = self.count_vectorizer.transform(UC_documents)
+            self.code_count_matrix = self.count_vectorizer.transform(code_documents)
+            
+        UC_words_count = self.UC_count_matrix.sum(axis=1)
+        code_words_count = self.code_count_matrix.sum(axis=1)
+
+        self.UC_count_matrix /= UC_words_count
+        self.code_count_matrix /= code_words_count
+
+        self.UC_count_matrix = self.UC_count_matrix.toarray()    
+        self.code_count_matrix = self.code_count_matrix.toarray()
+        return self.UC_count_matrix, self.code_count_matrix
         
 
     # Jensen-Shannon.
-    def JensenShannon(self, UC_documents: list, code_documents: list, train_or_test: str) -> np.ndarray:
+    def JensenShannon(self, UC_count_matrix, code_count_matrix) -> np.ndarray:
         
-        code_count_matrix = np.zeros((len(code_documents), len(code_documents[0])))
-        UC_count_matrix = np.zeros((len(UC_documents),len(UC_documents[0])))
-        
-        if train_or_test == 'train':
-            UC_count_matrix = self.count_vectorizer.fit_transform(UC_documents)
-            code_count_matrix = self.count_vectorizer.fit_transform(code_documents)
-        else:
-            UC_count_matrix = self.count_vectorizer.transform(UC_documents)
-            code_count_matrix = self.count_vectorizer.transform(code_documents)
-            
-        UC_words_count = UC_count_matrix.sum(axis=1)
-        code_words_count = code_count_matrix.sum(axis=1)
-
-        UC_count_matrix /= UC_words_count
-        code_count_matrix /= code_words_count
-
-        UC_count_matrix = UC_count_matrix.toarray()    
-        code_count_matrix = code_count_matrix.toarray()
         UC_count_matrix_repeated = np.repeat(UC_count_matrix, code_count_matrix.shape[0], axis=0)
         code_count_matrix_repeated = np.tile(code_count_matrix, (UC_count_matrix.shape[0],1))
 
         JS_matrix = pow(jensenshannon(UC_count_matrix_repeated, code_count_matrix_repeated, axis=1), 2)
         JS_matrix = JS_matrix.reshape(UC_count_matrix.shape[0], code_count_matrix.shape[0])
+
         #------------------------------loop approach---------------------------#
         # loop-approach
         # JS_matrix = np.zeros((UC_count_matrix.shape[0], code_count_matrix.shape[0]))
