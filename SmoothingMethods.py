2e9d7270bc50fcec6c106c31f8923ee96c15c2ba
yasmiinezaki <71704853+yasmiinezaki@users.noreply.github.com>
2024-02-23 10:53:03 +0200

    update smoothing

--- SmoothingMethods.py
+++ SmoothingMethods.py
@@ -31,8 +31,9 @@ code_term_frequency_in_document = []
 code_tokens_list = []
 code_term_document_frequency_in_corpus = Counter()
 
-total_JM = list()
-total_DP = list()
+JM_score = np.zeros((116,58))
+DP_score = np.zeros((116,58))
+code_file_count = 0
 
 for filename in os.listdir("./CC"):
     
@@ -45,31 +46,43 @@ for filename in os.listdir("./CC"):
     code_term_document_frequency_in_corpus += frequencies
     
     code_tokens = set(code_tokens)
-    JM_score = np.zeros(len(code_tokens))
-    DP_score = np.zeros(len(code_tokens))
+
     
-    for j,term in enumerate(code_tokens):
-        for i,doc in enumerate(uc_term_frequency_in_document):
+    for i,doc in enumerate(uc_term_frequency_in_document):
+        for j,term in enumerate(code_tokens):
             term_count_in_document = 0 if (uc_term_frequency_in_document[i] or {}).get(term) is None else (uc_term_frequency_in_document[i] or {}).get(term)
             corpus_frequency = 0 if uc_term_document_frequency_in_corpus.get(term) is None else uc_term_document_frequency_in_corpus.get(term) / uc_total_terms_in_corpus
-            JM_score[j] = ((1 - _lambda) * (term_count_in_document)) + (_lambda * uc_document_length[i] * corpus_frequency)
+            JM_score[code_file_count][i] += ((1 - _lambda) * (term_count_in_document)) + (_lambda * uc_document_length[i] * corpus_frequency)
 
             # Bayesian smoothing using Dirichlet priors. 
 
             # pµ(w|d) = c(w;d)+µ*p(w|C) / sum(c(w;d))+µ
             # the document term frequency c(w;d)
-            DP_score[j] = (term_count_in_document + mu * corpus_frequency) / (uc_document_length[i] + mu)
-    total_JM.append(JM_score)
-    total_DP.append(DP_score)
+            DP_score[code_file_count][i] += (term_count_in_document + mu * corpus_frequency) / (uc_document_length[i] + mu)
+    code_file_count += 1
 
 code_total_terms_in_corpus = sum(code_document_length)
 
+JM_uc = np.zeros((58,116))
+DP_uc = np.zeros((58,116))
+
+code_file_count = 0
+
 for uc in uc_term_frequency_in_document:
-    for j,term in enumerate(uc):
-        for i,doc in enumerate(code_term_frequency_in_document):
+    for i,doc in enumerate(code_term_frequency_in_document):
+        for j,term in enumerate(uc):
             term_count_in_document = 0 if (code_term_frequency_in_document[i] or {}).get(term) is None else (code_term_frequency_in_document[i] or {}).get(term)
             corpus_frequency = 0 if code_term_document_frequency_in_corpus.get(term) is None else code_term_document_frequency_in_corpus.get(term) / code_total_terms_in_corpus
-            JM_score = ((1 - _lambda) * (term_count_in_document)) + (_lambda * code_document_length[i] * corpus_frequency)
-            DP_score = (term_count_in_document + mu * corpus_frequency) / (code_document_length[i] + mu)
-    total_JM.append(JM_score)
-    total_DP.append(DP_score)
+            JM_uc[code_file_count][i] += ((1 - _lambda) * (term_count_in_document)) + (_lambda * code_document_length[i] * corpus_frequency)
+            DP_uc[code_file_count][i] += (term_count_in_document + mu * corpus_frequency) / (code_document_length[i] + mu)
+    code_file_count += 1
+
+# transpose the matrix and append
+
+JM_uc = JM_uc.transpose()
+DP_uc = DP_uc.transpose()
+JM_score = np.append(JM_score, JM_uc, axis=0)
+DP_score = np.append(DP_score, DP_uc, axis=0)
+
+print(JM_score.shape)
+print(DP_score.shape)
