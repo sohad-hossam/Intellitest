996c659e475149a82e13cbc042e4db449e59def2
yasminmessycodes <yasminh@silverkeytech.com>
2024-03-09 22:47:16 +0200

    Maintainbility score very very dead

--- GP GUI Base/electron-react-app/src/MaintainabilityScores.js
+++ GP GUI Base/electron-react-app/src/MaintainabilityScores.js
@@ -4,13 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Header } from './TopBar/TopBar';
 import { PageTitle } from './PageTitle/PageTitle';
 
-function ProgressBar({ score }) {
+function ProgressBar({ progress }) {
   return (
     <div className="progress-container">
       <div
         className="progress-bar"
-        style={{
-          width: `${score}%`,
+        style={{ 
+          width: `${progress}%`, 
           backgroundColor: '#092635'
         }}
       ></div>
@@ -18,19 +18,31 @@ function ProgressBar({ score }) {
   );
 }
 
+function DisplayScores({ label, score }) {
+  return (
+    <div className="DisplayScores">
+      <div className="ScoreLabel">{label}</div>
+      <div class="my-2"></div>
+      <ProgressBar progress={score} />
+    </div>
+  );
+}
+
 export function MaintainabilityScore() {
   const visibleHyperlinks = ['Home', 'About Us', 'Maintainability Scores', 'Trace Links'];
-
   const scores = {
-    label1: 20,
+    label: 30,
     label2: 80,
-    label3: 40
+    label3: 50,
   };
 
   return (
     <div className="App">
       <Header visibleHyperlinks={visibleHyperlinks} activeLink="Maintainability Scores" />
       <PageTitle title={"Maintainability Scores"} />
-     </div>
+      {Object.keys(scores).map((label, index) => (
+        <DisplayScores key={index} label={label} score={scores[label]} />
+      ))}
+    </div>
   );
 }
