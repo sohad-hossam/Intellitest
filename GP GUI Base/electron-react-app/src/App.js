154a9b5d113c72e4ce465a3725ab6e6cfa3fd6f5
sohad-hossam <sohad95husam@gmail.com>
2024-03-12 02:24:46 +0200

    Revert "Revert "Home page background added""
    This reverts commit 27f2d15799161fb9ffbed657ac9a2ba4f591ce82.

--- GP GUI Base/electron-react-app/src/App.js
+++ GP GUI Base/electron-react-app/src/App.js
@@ -1,17 +1,27 @@
-//import React, { useEffect, useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-//import { Header } from './TopBar/TopBar';
-//import { PageTitle } from './PageTitle/PageTitle';
-import { ImportProject } from './ImportProject';
-import { MaintainabilityScore } from './MaintainabilityScores';
-import { HomePage } from './HomePage';
-import { AboutUs } from './AboutUs';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { ImportProject } from "./ImportProject";
+import { MaintainabilityScore } from "./MaintainabilityScores";
+import { HomePage } from "./HomePage";
+import { AboutUs } from "./AboutUs";
+import { HashRouter, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
-     <div className="App">
-      <MaintainabilityScore/>
+    <HashRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" exact element={<HomePage />} />
+          <Route path="/AboutUs" component={AboutUs} />
+          <Route
+            path="/MaintainabilityScore"
+            component={MaintainabilityScore}
+          />
+          <Route path="/ImportProject" element={<ImportProject />} />
+        </Routes>
       </div>
-    );
+    </HashRouter>
+  );
 }
 
 export default App;
