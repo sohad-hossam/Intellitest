f789b889e174f18166ab49765e685288843546ba
sohad-hossam <sohad95husam@gmail.com>
2024-05-06 20:25:57 +0300

    Setting ML script for app

--- GP GUI Base/electron-react-app/src/MLScript.py
+++ GP GUI Base/electron-react-app/src/MLScript.py
@@ -5,15 +5,22 @@ class TraceLinks():
     def __init__(self, code_file: str, usecase_file: str) -> None:
         self.code_file = code_file
         self.usecase_file = usecase_file
+        ############### Currently the test files only ##################
+        self.CC_to_index = load('pickles/CCindex_test.pkl')
+        self.UC_to_index = load('pickles/UCindex_testpkl')
+        self.feature_map = load('/pickles/features_links_selected_test.pkl')
 
-    def featuremap(self, code_file: str, usecase_file: str) -> None:
+    def featureMap(self, code_file: str, usecase_file: str) -> None:  
+        CC_index = self.CC_to_index[code_file]
+        UC_index = self.CC_to_index[usecase_file]
         
-        pass
+        return self.feature_map[CC_index][UC_index]
+        
 
     def computeTraceLinks(self) -> int:
       
-        data = self.featuremap(self.code_file, self.usecase_file)
+        data = self.featureMap(self.code_file, self.usecase_file)
         model = load('pickles/teiid_model.pkl')
-        predictions = model.predict(data)
+        predictions = model.predict(data)[0]
         return predictions
 
