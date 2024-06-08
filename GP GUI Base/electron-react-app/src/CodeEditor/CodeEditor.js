dbd35fe3b9db8423ac1d55434cbe7d063f2ce2a4
yasminmessycodes <yasmin.shmila01@eng-st.cu.edu.eg>
2024-06-08 21:58:22 +0300

    highlight error line

--- GP GUI Base/electron-react-app/src/CodeEditor/CodeEditor.js
+++ GP GUI Base/electron-react-app/src/CodeEditor/CodeEditor.js
@@ -1,29 +1,8 @@
 import React, { useState } from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { a11yDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import { base16AteliersulphurpoolLight } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import { cb } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import { coldarkCold } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import { coldarkDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import { coy } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import { coyWithoutShadows } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import { darcula } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import { duotoneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import { duotoneEarth } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import { duotoneForest } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import { duotoneLight } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import { duotoneSea } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import { duotoneSpace } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import { funky } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import { ghcolors } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import { gruvboxDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
-
-
-const CodeEditor = ({ content, fileType }) => {
+const CodeEditor = ({ content, fileType, highlightLines = [] }) => {
 
   const [error, setError] = useState(null); 
 
@@ -41,15 +20,16 @@ const CodeEditor = ({ content, fileType }) => {
   };
 
   const language = getLanguageForFileType(fileType);
+
   return (
     <div>
       {error ? (
         <div>Error: {error.message}</div>
       ) : (
         <SyntaxHighlighter
-        language={language}
-        style={dracula}
-        customStyle={{
+          language={language}
+          style={dracula}
+          customStyle={{
             fontSize: '14px',
             lineHeight: '1.5',
             backgroundColor:  'rgba(15, 42, 42, 0.9)', 
@@ -58,15 +38,20 @@ const CodeEditor = ({ content, fileType }) => {
             overflowX: 'auto', 
             maxHeight: '900px', 
             overflowY: 'auto', 
-
           }}
-        showLineNumbers = {true}
-        PreTag="div" 
-      >
-        {content}
-      </SyntaxHighlighter>
-      
-      
+          showLineNumbers={true}
+          wrapLines={true}
+          lineProps={lineNumber => {
+            let style = { display: 'block' };
+            if (highlightLines.includes(lineNumber)) {
+              style.backgroundColor = 'rgba(255, 0, 0, 0.3)'; 
+            }
+            return { style };
+          }}
+          PreTag="div" 
+        >
+          {content}
+        </SyntaxHighlighter>
       )}
     </div>
   );
